8fd46ec093c014e8176fdda5eea9c0d13daf6ce5
akupila <akupila@gmail.com>
2016-01-12 11:06:18 +0100

    Testing deploy

--- app1/index.js
+++ app1/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('App 1');
+  res.send('App 1 :)');
 });
 
 app.listen(process.env.PORT, function () {
