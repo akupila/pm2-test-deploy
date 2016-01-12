2dd20fbd28bc3f7418685fec230acd27ff8fed22
akupila <akupila@gmail.com>
2016-01-12 10:53:16 +0100

    Fixed express stuff

--- app1/index.js
+++ app1/index.js
@@ -1,13 +1,10 @@
-// Load the http module to create an http server.
-var http = require('http');
+var express = require('express');
+var app = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<h1>App 1</h1>\n");
+app.get('/', function (req, res) {
+  res.send('App 1');
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(process.env.PORT, function() {
-  console.log('App 1 listening on port ' + process.env.PORT);
-});
+app.listen(process.env.PORT, function () {
+  console.log(`App 1 listening on port ${process.env.PORT}`);
+})
--- app2/index.js
+++ app2/index.js
@@ -1,13 +1,10 @@
-// Load the http module to create an http server.
-var http = require('http');
+var express = require('express');
+var app = express();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<h1>App 2</h1>\n");
+app.get('/', function (req, res) {
+  res.send('App 2');
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(process.env.PORT, function() {
-  console.log('App 2 listening on port ' + process.env.PORT);
-});
+app.listen(process.env.PORT, function () {
+  console.log(`App 2 listening on port ${process.env.PORT}`);
+})
