5cda59ca6af2b8986912159962880031bb9915ad
akupila <akupila@gmail.com>
2016-01-12 10:45:35 +0100

    port as env var

--- app2/index.js
+++ app2/index.js
@@ -4,8 +4,10 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<h1>Hello World 2</h1>\n");
+  response.write("<h1>App 2</h1>\n");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(process.env.PORT, function() {
+  console.log('App 2 listening on port ' + process.env.PORT);
+});
